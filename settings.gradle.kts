1df6d46e35526d88df661d8a272f6797bc4aad47
Josh R <atticus183@gmail.com>
2022-02-21 23:36:31 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ExpenseTrackerKMM"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/iosMain/kotlin/com/example/expensetrackerkmm/Platform.kt
@@ -0,0 +1,7 @@
+package com.example.expensetrackerkmm
+
+import platform.UIKit.UIDevice
+
+actual class Platform actual constructor() {
+    actual val platform: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}

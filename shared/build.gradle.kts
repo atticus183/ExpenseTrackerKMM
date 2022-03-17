3072a33cb9210bce19e20daaf95bc4af6d9f858c
Josh Rondestvedt <atticus183@gmail.com>
2022-03-16 22:01:38 -0500

    Adds KMP-NativeCoroutines library
    A library to use Kotlin Coroutines from Swift code in KMP apps.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("com.rickclephas.kmp.nativecoroutines") version "0.11.3"
 }
 
 kotlin {
@@ -17,7 +18,11 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
